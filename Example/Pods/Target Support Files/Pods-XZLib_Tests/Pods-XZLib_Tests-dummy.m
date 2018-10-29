53907202013eff7ea8acc4963558ca617962add8
zhuxuezhen@offcn.com <zhuxuezhen@offcn.com>
2018-10-29 20:18:25 +0800

    0.1.0

---
+++ Example/Pods/Target Support Files/Pods-XZLib_Tests/Pods-XZLib_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XZLib_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_XZLib_Tests
+@end
---
+++ Example/Pods/Target Support Files/XZLib/XZLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XZLib : NSObject
+@end
+@implementation PodsDummy_XZLib
+@end
